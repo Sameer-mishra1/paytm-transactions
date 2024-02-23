88eede946491583af244ba549b1cc4995edfb1df
Sameer-mishra1 <77093405+Sameer-mishra1@users.noreply.github.com>
2024-02-23 23:20:04 +0530

    Added frontend - Sign up page & several components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Dashboard } from './pages/Dashboard'
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter >
+        <Routes>
+          <Route path='/signup' element={<Signup />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+export function Dashboard() {
+    return <div>
+        Hi there
+    </div>
+}
